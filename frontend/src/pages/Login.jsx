9def8899ab5f07775e47f596179a22cf1b933240
reshma-devraj <reshamdevraj2230@gmail.com>
2025-02-10 18:17:40 +0530

    m10

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Login from '../components/Login/Login';
+
+const Loginpage = () => {
+    return (
+        <div>
+            <Login />
+        </div>
+    )
+}
+
+export default Loginpage
---
+++ frontend/src/routes/routes.js
@@ -0,0 +1,7 @@
+import LoginPage from "../pages/Login";
+import SignupPage from "../pages/SignupPage";
+import Home from "../pages/Home";
+import CreateProduct from "../pages/createProduct";
+
+export{
+    LoginPage,SignupPage,Home,CreateProduct}
