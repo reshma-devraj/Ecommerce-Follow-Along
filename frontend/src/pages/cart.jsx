059790bcea3606390d2b8ea968cd27d9821caf7b
reshma-devraj <reshmadevraj2230@gmail.com>
2025-02-20 17:52:14 +0530

    16m

---
+++ frontend/src/pages/cart.jsx
@@ -0,0 +1,14 @@
+import Nav from '../components/nav'
+
+const cart = () => {
+  return (
+  <>
+    <Nav />
+    <div>
+        <h1>Cart</h1>
+    </div>
+    </>
+  );
+}
+
+export default cart
---
+++ frontend/src/routes/routes.js
@@ -0,0 +1,10 @@
+import LoginPage from "../pages/Login";
+import SignupPage from "../pages/SignupPage";
+import Home from "../pages/Home";
+import CreateProduct from "../pages/createProduct";
+import MyProducts from "../pages/myProduct";
+import Cart from "../pages/cart";
+
+
+export{
+    LoginPage,SignupPage,Home,CreateProduct,MyProducts,Cart}
